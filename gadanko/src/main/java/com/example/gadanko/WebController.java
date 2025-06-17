274d901834502e97a2e2e57470d2234900dc615e
emil-stolecki <emilstolecki@proton.me>
2025-06-17 10:42:56 +0200

    test

--- gadanko/src/main/java/com/example/gadanko/WebController.java
+++ gadanko/src/main/java/com/example/gadanko/WebController.java
@@ -1,4 +1,22 @@
 package com.example.gadanko;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class WebController {
+
+    @GetMapping("/profile")
+    public String profile(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "profile";
+    }
+    @GetMapping("/")
+    public String home() {
+
+        return "index";
+    }
+
 }
