bf7b7082fc560f1969664e5247c3a3ac6a37e7cb
emil-stolecki <emilstolecki@proton.me>
2025-06-19 23:04:11 +0200

    design home page

--- gadanko/src/main/java/com/example/gadanko/WebController.java
+++ gadanko/src/main/java/com/example/gadanko/WebController.java
@@ -8,15 +8,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
+
+    @GetMapping("/")
+    public String start() {
+        return "index";
+    }
+
+
+    @GetMapping("/home")
+    public String home() {
+        //check if logged in
+        return "mainpage";
+    }
+
+    @GetMapping("/chat/id")
+    public String getChat() {
+        //check if logged in
+        //get chat data
+        return "chat";
+    }
+
+
+
+
+
     @GetMapping("/profile")
     public String profile(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "profile";
-    }
-    @GetMapping("/")
-    public String home() {
-
-        return "index";
     }
 
 }
