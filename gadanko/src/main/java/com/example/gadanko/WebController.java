742cc81880c2083b9c9a0ca03bf61f85910d08fd
emil-stolecki <emilstolecki@proton.me>
2025-07-29 11:57:44 +0200

    added spring security

--- gadanko/src/main/java/com/example/gadanko/WebController.java
+++ gadanko/src/main/java/com/example/gadanko/WebController.java
@@ -89,19 +89,8 @@ public class WebController {
     public String getChat(@PathVariable Long id, Model model) {
         //check if logged in
         //get chat data
-        Message[] mock_messages = new Message[10];
-        mock_messages[0]=new Message("user1", "20:06 22-06-2025","AAAA");
-        mock_messages[1]=new Message("user2", "20:06 22-06-2025", "UWU");
-        mock_messages[2]=new Message("user1", "20:06 22-06-2025","OwO");
-        mock_messages[3]=new Message("user3", "20:06 22-06-2025","LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT");
-        mock_messages[4]=new Message("user1", "20:06 22-06-2025",",XDDDDDDD");
-        mock_messages[5]=new Message("user2", "20:06 22-06-2025","ok");
-        mock_messages[6]=new Message("user3", "20:06 22-06-2025","kys");
-        mock_messages[7]=new Message("user1", "20:06 22-06-2025","no u");
-        mock_messages[8]=new Message("user2", "20:06 22-06-2025","ur mom");
-        mock_messages[9]=new Message("user3", "20:06 22-06-2025","bey");
-
-        model.addAttribute("mockchat",mock_messages);
+        ArrayList<Message> messages = new ArrayList<Message>();
+        model.addAttribute("chat",messages);
         model.addAttribute("newMessage","");
 
         Group[] groups = getGroups();
@@ -135,11 +124,11 @@ public class WebController {
         //int messagesToRetrieve = 10;
         //long startIndex = max(consumer.position(topicPartition) - messagesToRetrieve,0);
         //consumer.seek(topicPartition, startIndex);
-        System.out.println("HERE");
         ConsumerRecords<Long, Message> records = consumer.poll(Duration.ofMillis(100));
         records.forEach(record -> {
             if(record.value()!=null){
                 System.out.println(record.value().getText());
+                messages.add(record.value());
             }
             else{
                 System.out.println("nuh uh");
